37d399678cc5cb58a0f54db3a77a76fa5ab73fec
casey cochran <ccochran@metiscg.com>
2022-12-08 10:47:23 -0500

    jquery-project

---
+++ jquery-project/index.js
@@ -0,0 +1,22 @@
+
+$(document).ready( () => {
+
+$('#even').on('click', () => {
+
+    $('.even-rows').toggleClass('even-highlight')
+
+})
+
+$('#odd').on('click', () => {
+
+    $('.odd-rows').toggleClass('odd-highlight')
+
+})
+
+
+
+
+
+
+
+})
